4483e425b7abcc8c9c6701de750dcf339e15b762
Akshay Rawat <akshay.rawat@checkr.com>
2022-06-27 10:04:01 -0700

    updates

--- neovim/.config/nvim/lua/keys.lua
+++ neovim/.config/nvim/lua/keys.lua
@@ -11,5 +11,7 @@ local options = {
 
 -- iterm: https://stackoverflow.com/questions/33060569/mapping-command-s-to-w-in-vim
 map("n", "<C-e>", '<cmd>lua require("telescope.builtin").buffers({ sort_mru = true })<CR>', options)
-map("n", "<C-p>", '<cmd>lua require("telescope.builtin").find_files({ hidden=true })<CR>', options)
+map("n", "<C-p>", '<cmd>lua require("telescope.builtin").find_files({ hidden = true })<CR>', options)
+
+-- does not work on hidden files
 map("n", "<C-f>", '<cmd>lua require("telescope.builtin").live_grep()<CR>', options)
