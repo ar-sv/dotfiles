e36d67c81136a1a6c1ed110e87eddd92b40a1c60
Akshay Rawat <akshay@clerky.com>
2026-03-25 15:36:14 -0700

    Neovim updates

--- packages/nvim/.config/nvim/init.lua
+++ packages/nvim/.config/nvim/init.lua
@@ -1,3 +1,15 @@
-vim.opt.clipboard = "unnamedplus"
-
 vim.cmd.colorscheme("retrobox")
+
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.scrolloff = 8
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.undofile = true
+vim.opt.clipboard = "unnamedplus"
